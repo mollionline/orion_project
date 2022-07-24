0bd314a0012a22744a0237cd4e2e66ae84a441bd
moldir <auezova.90@mail.ru>
2022-07-24 11:27:12 +0600

    Начальная загрузка проекта

---
+++ api_v1/views.py
@@ -0,0 +1,31 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
+
+
+# Create your views here.
+
+class UserPermission(permissions.BasePermission):
+    message = 'У вас нет прав доступа!'
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if obj.user == request.user:
+            return True
+        return False
+
+
+class LogoutView(APIView):
+    permission_classes = []
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        if user.is_authenticated:
+            user.auth_token.delete()
+        return Response({'status': 'ok'})
