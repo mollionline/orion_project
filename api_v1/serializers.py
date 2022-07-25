951e8e61cea48ee5d128d5716b679e1fc22be69c
moldir <auezova.90@mail.ru>
2022-07-25 10:01:27 +0600

    feat: Добавила возможность авторизации по токену на 7 дней
    Добавила возможность авторизации по токену на 7 дней, добавила просмотр юзера после авторизации с временем жизни токена
    
    Связано:#1

---
+++ api_v1/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+
+class UserSigninSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+
+class UserSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,2 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from .views import LogoutView, user_info, SignInAPIView
 
+urlpatterns = [
+    path('login/', SignInAPIView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='api_token_delete'),
+    path('user_info/', user_info)
+]
