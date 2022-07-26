cc7b9b4aac7e4c6d1358b45de853732d2d1142fd
moldir <auezova.90@mail.ru>
2022-07-26 18:48:04 +0600

    refactor: Скрыла в сеттингах секрет кей
    Скрыла в сеттингах секрет кей

--- core/settings.py
+++ core/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-phujn=v24hypcjsv#t0(s69^%xd@xubs)lo+^a7u!cwytzz2we'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
